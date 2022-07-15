0699a14dc413959ea490f5ad0522fcb372dc0777
mundoenrique <mundoenrique@hotmail.com>
2022-07-15 00:13:11 -0400

    Nueva pagina

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,9 @@
+import { Layout } from '../../components/layouts';
+
+export default function FavoritePage() {
+  return (
+    <Layout title="Algún pokemon">
+      <h1>Favoritos</h1>
+    </Layout>
+  );
+}
