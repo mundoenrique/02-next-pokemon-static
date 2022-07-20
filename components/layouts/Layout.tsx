20bf1229f43f0c2bd42986e8826a4a9396987d28
mundoenrique <mundoenrique@hotmail.com>
2022-07-20 11:54:54 -0400

    Meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -5,6 +5,7 @@ interface Props {
   title?: string;
   children: JSX.Element;
 }
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
 
 export function Layout({ title, children }: Props) {
   return (
@@ -17,6 +18,12 @@ export function Layout({ title, children }: Props) {
           content={`Información sobre el pokemón ${title}`}
         />
         <meta name="kewords" content={`${title}, pokemon, pokedex`} />
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta es la pagina sobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
       <Navbar />
       <main
